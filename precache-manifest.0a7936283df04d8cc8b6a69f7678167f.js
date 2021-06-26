f0bcc0ef9fd2998582cbe0b8916908eecc00d242
Venkateshe <kolapallinagavenkatesh@gmail.com>
2021-06-26 14:41:57 +0530

    Updates

---
+++ precache-manifest.0a7936283df04d8cc8b6a69f7678167f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a8a495c94e5a464307cfc22a5760b5",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "9ebf20563e7d7a18d444",
+    "url": "/login/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9ebf20563e7d7a18d444",
+    "url": "/login/static/js/2.b1ed7d04.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/login/static/js/2.b1ed7d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758753e4db9d6ce6a857",
+    "url": "/login/static/js/main.407501e2.chunk.js"
+  },
+  {
+    "revision": "8121af70904d810178ea",
+    "url": "/login/static/js/runtime-main.0412cc64.js"
+  }
+]);
